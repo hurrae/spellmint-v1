7a1bedb13e375a038d519aa27a8be102f2cef98e
bhuvan-b <bokkabhuvan2@gmail.com>
2023-04-12 02:04:59 +0530

    Added Create Project Modal

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,17 @@
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
+      <>
+        <Script
+          src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"
+          strategy="beforeInteractive"
+        />
+      </>
     </SessionProvider>
   );
 }
