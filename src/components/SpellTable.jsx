3a8ef31aadb31afbab3ef735ee2937c11d59ec04
bhuvan-b <bokkabhuvan2@gmail.com>
2023-06-12 00:30:01 +0530

    Spell Rename Done

--- src/components/SpellTable.jsx
+++ src/components/SpellTable.jsx
@@ -145,7 +145,7 @@ const SpellTableRow = ({
           <ul class="py-1" role="none">
             <li>
               <a
-                href="#"
+                href={router.asPath + `/${name}`}
                 class="block px-4 py-2 text-base text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
                 role="menuitem"
               >
